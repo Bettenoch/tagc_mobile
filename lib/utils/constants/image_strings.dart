0737ea0d4aa31f0aab967c4d526e532534b63dbc
Bettenoch <bettenoch254@gmail.com>
2024-07-20 12:30:10 +0300

    add constant image strings.

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,41 @@
+class TImages {
+  // App Logos
+  static const String darkAppLogo = "assets/logos/logo1.png";
+  static const String lightAppLogo = "assets/logos/logo2.png";
+
+  //Social Logos
+  static const String google = "assets/icons/google.svg";
+  static const String facebook = "assets/icons/facebook.svg";
+  static const String twitter = "assets/icons/twitter.svg";
+  static const String tikTok = "assets/icons/tiktok.svg";
+  static const String instagram = "assets/icons/instagram.svg";
+
+  static const String onBoardingImage1 = "assets/images/anime1.gif";
+  static const String onBoardingImage2 = "assets/images/anime3.gif";
+  static const String onBoardingImage3 = "assets/images/delivery.gif";
+  static const String onBoardingImage4 = "assets/images/delivery2.gif";
+
+  //random images
+
+  static const String icon1 = "assets/icons/alarm-clock.png";
+  static const String icon2 = "assets/icons/binder-clip.png";
+  static const String icon3 = "assets/icons/book.png";
+  static const String icon4 = "assets/icons/calendar.png";
+  static const String icon5 = "assets/icons/laptop-computer.png";
+  static const String icon6 = "assets/icons/laptop-screen.png";
+  static const String icon7 = "assets/icons/trophy.png";
+  static const String icon8 = "assets/icons/web-design.png";
+
+  //promotions
+  static const String promoIcon1 = "assets/promos/promopic1.png";
+  static const String promoIcon2 = "assets/promos/promopic2.png";
+  static const String promoIcon3 = "assets/promos/promopic3.png";
+  static const String promoIcon4 = "assets/promos/promopic4.png";
+
+  //laptops
+  static const String laptop1 = "assets/laptops/laptop1.png";
+  static const String laptop2 = "assets/laptops/laptop2.png";
+  static const String laptop3 = "assets/laptops/laptop3.png";
+  static const String laptop4 = "assets/laptops/laptop4.png";
+  static const String laptop5 = "assets/laptops/laptop5.png";
+}

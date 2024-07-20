4b82a083a1f549895e5b5f038ecea5b499eb4608
Bettenoch <bettenoch254@gmail.com>
2024-07-20 12:29:57 +0300

    add constant enumerators.

---
+++ lib/utils/constants/enums.dart
@@ -0,0 +1,5 @@
+// enums cannot be created inside of a class
+
+enum TextSizes { small, medium, large}
+enum OrderStatus { processing, shipped, delivered}
+enum PaymentMethods {paypal, googlePay, applePay, visa, masterCard, creditCard, payStack, razorPay, paytm}

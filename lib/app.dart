f0aed9d8f4196eac2c5472aad0247b1bb65c3356
Bettenoch <bettenoch254@gmail.com>
2024-07-20 11:55:22 +0300

    Test HomePage G

--- lib/app.dart
+++ lib/app.dart
@@ -30,7 +30,7 @@ class MyHomePage extends StatelessWidget {
         title: const Text('Home Page'),
       ),
       body: const Center(
-        child: Text('Hello Test, world! TEST'),
+        child: Text('Hello Test, MISTRAL! TEST'),
       ),
     );
   }
